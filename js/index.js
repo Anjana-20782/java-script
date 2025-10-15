0f86db1da55425f37192c1e89017684fc556bc69
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-15 15:28:44 +0530

    new contents adddee

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@
 //  console.log(X);     //X is not defined
 
 
- let a="";
- console.log(Boolean(a));    //false
+//  let a="";
+//  console.log(Boolean(a));    //false
  
+
+// let a=3;
+// console.log(Boolean(a));    //true
+
+
+let a=0;
+console.log(Boolean(a));   //false
