12f6aa178ca185b6245c3d705eb0f99901abfce8
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-15 15:40:30 +0530

    new contents added

--- js/index.js
+++ js/index.js
@@ -3,10 +3,14 @@
 // let x=10;
 //  console.log(X);     //X is not defined
 
-let a=5+ +"5";
-console.log(a);    //10
+// let a=5+ +"5";
+// console.log(a);    //10
 
+// let a=5+"5";
+// console.log(a);    //55
 
+// let a=5-"5"
+// console.log(a);      //0
 
 
 // 2)Datatypes
@@ -21,3 +25,18 @@ console.log(a);    //10
 
 // let a=0;
 // console.log(Boolean(a));   //false
+
+let a=55;
+let b=56;
+console.log(b);         
+
+console.log(String(a));  //55
+
+
+
+// 3)loop
+
+// for(i=1;i<=5;i++)
+// {
+//     console.log("hai").       //5.hai
+// }
