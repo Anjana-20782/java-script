fbfb2000aecdf0818b23170ac82f0e5956707b61
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-15 15:32:05 +0530

    new contsnts added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+// 1)Variable
+
 // let x=10;
 //  console.log(X);     //X is not defined
 
+let a=5+ +"5";
+console.log(a);    //10
+
+
+
+
+// 2)Datatypes
 
 //  let a="";
 //  console.log(Boolean(a));    //false
@@ -10,5 +19,5 @@
 // console.log(Boolean(a));    //true
 
 
-let a=0;
-console.log(Boolean(a));   //false
+// let a=0;
+// console.log(Boolean(a));   //false
